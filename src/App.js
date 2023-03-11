d923a5f49a10b8ed08040f482cf355d836337736
thakuratul2 <pratapsinghatul111@gmail.com>
2023-03-11 10:28:47 +0530

    Constructor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Content from './Components/Content'
 import Navbar from './Components/Navbar'
 
 
-function App() {
-
-  function getData(){
-    alert("Button Was Clicked");
+class App extends React.Component{
+ constructor(){
+  super();
+  this.state={
+    msg: "Click Me"
   }
-  return (
-    <Fragment>
-    <Navbar title="React Learn[Atul Pratap Singh]" aboutTitle="About Us" contactTitle="Contact Us"/>
-
-    <Content data={getData}/>
-    </Fragment>
+ }
+render(){
+  return(
+    <>
+      <Navbar title="React Learning [Atul Pratap Singh]" aboutTitle="About Us" contactTitle="Contact Us"/>
+      <Content />
+      <h1>{this.state.msg}</h1>
+    </>
   )
+}
 }
 
 export default App
